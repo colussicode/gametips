308528bdef02f0c91a3980355f2844eeba8de7f8
colussicode <colussidev@gmailcom>
2020-07-28 13:18:04 -0300

    started to use styled-components

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import Logo from '../../assets/images/Logo.png'
+import Logo from '../../assets/images/Logo.png';
 import './Menu.css';
-import ButtonLink from '../components/ButtonLink';
+import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
-function Menu () {
-    return (
-        <nav className="Menu">
-            <a href="/">
-            <img src={Logo} alt="Logo" className="Logo" />
-            </a>
+function Menu() {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="AluraFlix logo" />
+      </a>
 
-            <ButtonLink className="ButtonLink" href="/">
-                
-            </ButtonLink>
-        </nav>
-    )
+      <Button as="a" className="ButtonLink" href="/">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
